c696dea4296f97e5296a990cd3d86a2dbe883fef
Simon Heybrock <simon.heybrock@esss.se>
2018-08-09 08:25:29 +0200

    Reference external script.

--- benchmarks/make_SNSPowderReduction_files.sh
+++ benchmarks/make_SNSPowderReduction_files.sh
@@ -13,6 +13,7 @@ cp $SOURCE/PG3_5226_event.nxs PG3_99999_event.nxs
 
 for i in 77777 88888 99999
 do
+  # From https://github.com/DMSC-Instrument-Data/nexus-sandbox
   python ~/code/nexus-sandbox/scripts/grow.py -s $SCALE -f PG3_"$i"_event.nxs
   h5repack -i PG3_"$i"_event.nxs -o PG3_"$i"_event-rechunked.nxs -l CHUNK=8192
   mv PG3_"$i"_event-rechunked.nxs PG3_"$i"_event.nxs
