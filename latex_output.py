77f8ab5132ca80bdfa8e254cbc16f8e52f89a71a
Simon Heybrock <simon.heybrock@esss.se>
2018-08-13 09:12:40 +0200

    More cleanup and clarifications.

--- latex_output.py
+++ latex_output.py
@@ -1,8 +1,8 @@
 def generate_latex_header():
     header = """\centering
 \\begin{tabular}{rlrrrrrrrrr}
-Pixels & Mode & $p$-beam & Use & $\Fevent$ & $\Trun$ & $\Treduction$ & $\\Ncore$ & $\langle\\Ncore\\rangle$ & $\\Mcore$ \\\\
-& & $[\mathrm{MW}]$ & [\%] & $\mathrm{[s^{-1}]}$ & $\mathrm{[s]}$ & $\mathrm{[s]}$ & & & [GByte]\\\\
+Pixels & Mode & $p$-beam & $p_{\\text{use}}$ & $\Fevent$ & $\Trun$ & $\Treduction$ & $\\Ncore$ & $\langle\\Ncore\\rangle$ & $\\Mcore$ \\\\
+& & $[\mathrm{MW}]$ & & $\mathrm{[s^{-1}]}$ & $\mathrm{[s]}$ & $\mathrm{[s]}$ & & & [GByte]\\\\
 \hline
 """
     return header
@@ -34,9 +34,9 @@ class LatexTabular:
         event_rate_base, exponent = float_str.split("e")
         event_rate_exponent = int(exponent)
         if t_reduction > 30 and t_run < self.speedup*t_reduction:
-            base = ('& {:4.1f} & {:2.1f} & ${}\cdot 10^{:1}$ & {:8.1f} & \\textcolor{{red}}{{{:6.0f}}} & {:4} & {:6.0f} & {:4.0f}' + '\\\\').format(power, 100*use_fraction, event_rate_base, event_rate_exponent, t_run, t_reduction, *args)
+            base = ('& {:4.1f} & {} & ${}\cdot 10^{:1}$ & {:8.1f} & \\textcolor{{red}}{{{:6.0f}}} & {:4} & {:6.0f} & {:4.0f}' + '\\\\').format(power, use_fraction, event_rate_base, event_rate_exponent, t_run, t_reduction, *args)
         else:
-            base = ('& {:4.1f} & {:2.1f} & ${}\cdot 10^{:1}$ & {:8.1f} & {:6.0f} & {:4} & {:6.0f} & {:4.0f}' + '\\\\').format(power, 100*use_fraction, event_rate_base, event_rate_exponent, t_run, t_reduction, *args)
+            base = ('& {:4.1f} & {} & ${}\cdot 10^{:1}$ & {:8.1f} & {:6.0f} & {:4} & {:6.0f} & {:4.0f}' + '\\\\').format(power, use_fraction, event_rate_base, event_rate_exponent, t_run, t_reduction, *args)
         if self.power == 0:
             self.lines.append(base + '\n')
         elif self.power == 4:
